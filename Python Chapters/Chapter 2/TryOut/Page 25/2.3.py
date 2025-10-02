1362b79e5b6df4fdcacf9e93cc5eb98467b8431a
josetaka79 <josetaka79@gmail.com>
2025-10-02 18:52:40 +0200

    Delete Python Chapters/Chapter 2 directory

--- Python Chapters/Chapter 2/TryOut/Page 25/2.3.py
+++
@@ -1,2 +0,0 @@
-var ="Josephine";
-print("Hello Josephine, ""Would you like to learn some Python today?" )
--- Python Chapters/Chapter 2/TryOut/Page 25/2.6.py
+++
@@ -1,3 +0,0 @@
-name="famous_person"
-message ="Education is the most powerful weapon which you can use to change the world."
-print(name + ", " + message)
